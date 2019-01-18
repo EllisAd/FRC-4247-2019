1e68c45e0fbca895152a18f998402445a930a79a
Nicholas Iannone <Nicholas.Iannone@MacGregorPartners.com>
2019-01-18 17:16:57 -0600

    Added TalonSRX code

--- src/main/java/frc/robot/subsystems/DriveTrain.java
+++ src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -1,19 +1,19 @@
 package frc.robot.subsystems;
 
-import edu.wpi.first.wpilibj.Talon;
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+
 import edu.wpi.first.wpilibj.command.Subsystem;
 import frc.robot.RobotMap;
 
 public class DriveTrain extends Subsystem {
 
-    // TODO Replace this with TalonSRX!
-    private final Talon leftMotor = new Talon(RobotMap.LEFT_MOTOR_CANBUS_PORT);
-    private final Talon rightMotor = new Talon(RobotMap.RIGHT_MOTOR_CANBUS_PORT);
+    private final TalonSRX leftMotor = new TalonSRX(RobotMap.LEFT_MOTOR_CANBUS_PORT);
+    private final TalonSRX rightMotor = new TalonSRX(RobotMap.RIGHT_MOTOR_CANBUS_PORT);
 
     public void set(double leftMotorOutput, double rightMotorOutput) {
-        // TODO Scaling?
-        leftMotor.set(leftMotorOutput);
-        rightMotor.set(rightMotorOutput);
+        leftMotor.set(ControlMode.PercentOutput, leftMotorOutput);
+        rightMotor.set(ControlMode.PercentOutput, rightMotorOutput);
     }
 
     @Override
